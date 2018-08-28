5542c0f149ead74ec138377adfaa0d12b3307735
Keaton Gallagher <d00249750@dmail.dixie.edu>
2018-08-28 17:57:08 -0600

    Have the first page pretty much done, I need to make so when one of the arrows is clicked my name changes to something else instead of keaton like works

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import HomeComponent from './components/Home';
 import registerServiceWorker from './registerServiceWorker';
 import './css/index.css';
+import './js/main.js';
 
 
 ReactDOM.render(<HomeComponent />, document.getElementById('home'));
